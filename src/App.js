4c666aa94ec0ceb18f9c536a7dc59d14931804df
mrsteveson <patrick.steveson727@gmail.com>
2019-07-31 16:45:51 -0500

    basic register/login functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-
+      <Register/>
+      <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
-function Login() {
-  return (
-    <div>
+class Register extends React.Component {
+  state = {
+    username: '',
+    password: '',
 
-    </div>
-  );
-}
+  }
 
-export default Login;
+  handleChange = event => {
+      this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const heroku = 'https://maze-mud-server.herokuapp.com'
+    axios
+      .post(`${heroku}/api/login/`, this.state)
+      .then(res => {
+        console.log(res.data);
+        const token = res.data.key;
+        localStorage.setItem('token', token)
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
+  };
+
+  render() {
+    return (
+      <div className='LoginContainer'>
+        <h2>Login Here</h2>
+        
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type = 'text'
+            name = 'username'
+            placeholder = 'Username'
+            value = {this.state.username}
+            onChange = {this.handleChange}
+          />
+
+          <input 
+            type = 'password'
+            name = 'password'
+            placeholder = 'Password Please'
+            value = {this.state.password}
+            onChange = {this.handleChange}
+          />
+          <button type='submit'>Login Submit</button>
+        </form>
+        {/* <Link to='/register'>Register</Link> */}
+      </div>
+    );
+  };
+};
+
+export default Register;
